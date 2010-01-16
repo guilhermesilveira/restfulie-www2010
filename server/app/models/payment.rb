4ccf70dee34d78fb5f021464594a590fe463dcb6
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-01-16 21:09:33 -0200

    with controller and model

--- server/app/models/payment.rb
+++ server/app/models/payment.rb
@@ -1,2 +1,14 @@
 class Payment < ActiveRecord::Base
+  
+  acts_as_restfulie
+  
+  media_type 'application/vnd.restbucks+xml'
+  
+  belongs_to :order
+  
+  def initialize(hash = {})
+    super(hash)
+    self.payment_date = Time.now unless self.payment_date
+  end
+  
 end
