c175ce5125a2aaeaa096b5b9c95aaebb8eb47611
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-01-16 21:38:02 -0200

    working with hashi

--- client/spec/main_spec.rb
+++ client/spec/main_spec.rb
@@ -24,47 +24,47 @@ context Restfulie do
   def create_order(where = "TO_TAKE")
     Restfulie.at('http://localhost:3000/orders').as('application/vnd.restbucks+xml').create(new_order(where))
   end
-      
-  it "should cancel an order and delete it from the database" do
-    order = create_order
-    order.web_response.is_successful?.should be_true
-    cancelled = order.cancel
-    cancelled.web_response.should be_is_successful
-    order.self.web_response.code.should == "404"
-  end
-  
-  it "should update an order while possible" do
-    order = create_order
-    order = order.update(new_order("EAT_IN"), :method => :put)
-    order.web_response.is_successful?.should be_true
-    order.location.should == "EAT_IN"
-  end
-  
-  it "should complain if partially paying" do
-    order = create_order
-    order.request.as('application/vnd.restbucks+xml').pay(payment(1)).web_response.code.should == "400"
-  end  
-  
-  it "should allow to pay" do
-    order = create_order
-    order.request.as('application/vnd.restbucks+xml').pay(payment(order.cost)).web_response.code.should == "200"
-    order = order.self
-    order.web_response.code.should == "200"
-    order.status.should == "preparing"
-  end
-    
-    it "should not allow cancel an order if already paid" do
-      order = create_order
-      order.request.as('application/vnd.restbucks+xml').pay(payment(order.cost))
-      order.cancel.web_response.code.should == "405"
-    end
-    
-    it "should not allow to pay twice" do
-      order = create_order
-      order.request.as('application/vnd.restbucks+xml').pay(payment(order.cost))
-      order.request.as('application/vnd.restbucks+xml').pay(payment(order.cost)).web_response.code.should == "405"
-    end
-      
+        
+       it "should cancel an order and delete it from the database" do
+         order = create_order
+         order.web_response.is_successful?.should be_true
+         cancelled = order.cancel
+         cancelled.web_response.should be_is_successful
+         order.self.web_response.code.should == "404"
+       end
+       
+       it "should update an order while possible" do
+         order = create_order
+         order = order.update(new_order("EAT_IN"), :method => :put)
+         order.web_response.is_successful?.should be_true
+         order.location.should == "EAT_IN"
+       end
+       
+       it "should complain if partially paying" do
+         order = create_order
+         order.request.as('application/vnd.restbucks+xml').pay(payment(1)).web_response.code.should == "400"
+       end  
+       
+       it "should allow to pay" do
+         order = create_order
+         order.request.as('application/vnd.restbucks+xml').pay(payment(order.cost)).web_response.code.should == "200"
+         order = order.self
+         order.web_response.code.should == "200"
+         order.status.should == "preparing"
+       end
+         
+         it "should not allow cancel an order if already paid" do
+           order = create_order
+           order.request.as('application/vnd.restbucks+xml').pay(payment(order.cost))
+           order.cancel.web_response.code.should == "405"
+         end
+         
+         it "should not allow to pay twice" do
+           order = create_order
+           order.request.as('application/vnd.restbucks+xml').pay(payment(order.cost))
+           order.request.as('application/vnd.restbucks+xml').pay(payment(order.cost)).web_response.code.should == "405"
+         end
+           
     it "should allow to take out and receive receipt" do
       order = create_order
       order.request.as('application/vnd.restbucks+xml').pay(payment(order.cost), :method => :post)
--- server/app/controllers/orders_controller.rb
+++ server/app/controllers/orders_controller.rb
@@ -10,8 +10,7 @@ class OrdersController < ApplicationController
     @model = model_type.find(params[:id])
     if @model.can? :cancel
       @model.delete
-      puts "IVE DELETED THIS GUY!!! #{@model.id}"
-      # head :ok
+      head :ok
     elsif @model.can? :retrieve
       @model.status = "delivered"
       @model.save!
@@ -28,24 +27,6 @@ class OrdersController < ApplicationController
     # model["items"].map do |item|
       # Item.new(item)
     # end
-  end
-
-  def update
-    @loaded = model_type.find(params[:id])
-    return head :status => 405 unless @loaded.can? :update
-
-    type = model_type
-    return head 415 unless fits_content(type, request.headers['CONTENT_TYPE'])
-
-    debugger
-    @model = Hash.from_xml(request.body.string)[model_name]
-    pre_update(@model) if self.respond_to?(:pre_update)
-    
-    if @loaded.update_attributes(@model)
-      render_resource @loaded
-    else
-      render :xml => @loaded.errors, :status => :unprocessable_entity
-    end
   end
 
 end
