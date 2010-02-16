8fd36b0e59c85dc3f2faff1dbfcccbbd60f91193
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-02-16 16:33:56 -0200

    capable of delivering with java

--- client/spec/main_spec.rb
+++ client/spec/main_spec.rb
@@ -5,7 +5,7 @@ context Restfulie do
   # how to process an order
   #
   # order = create_order
-  # order.request.as('application/vnd.restbucks+xml').pay(payment(order.cost), :method => :post)
+  # order.request.as('application/vnd.restbucks+xml').payment(payment(order.cost), :method => :post)
   # sleep 20
   # order.self.retrieve(:method => :delete)
   # receipt = order.self.receipt
@@ -30,77 +30,83 @@ context Restfulie do
     order.web_response.should be_is_successful
   end
 
-  # it "should allow crawling children" do
-  #   order = create_order
-  #   items = order.items(:method => :get)
-  #   items[1].drink.should == "latte"
-  # end
-  # 
-  # it "should load from cache" do
-  #   order = create_order
-  #   previous_response = order.self.web_response
-  #   order.self.web_response.should == previous_response
-  # end
-  # 
-  #  it "should cancel an order and delete it from the database" do
-  #    order = create_order
-  #    cancelled = order.cancel
-  #    cancelled.web_response.should be_is_successful
-  #    order.self.web_response.code.should == "404"
-  #  end
-  #  
-  #  it "should update an order while possible" do
-  #    order = create_order
-  #    order = order.update(new_order("EAT_IN"), :method => :put)
-  #    order.web_response.is_successful?.should be_true
-  #    order.location.should == "EAT_IN"
-  #  end
-  #  
-  #  it "should complain if partially paying" do
-  #    order = create_order
-  #    order.request.as('application/vnd.restbucks+xml').pay(payment(1)).web_response.code.should == "400"
-  #  end  
-  #  
-  #  it "should allow to pay" do
-  #    order = create_order
-  #    order.request.as('application/vnd.restbucks+xml').pay(payment(order.cost)).web_response.code.should == "200"
-  #    order = order.self
-  #    order.web_response.code.should == "200"
-  #    order.status.should == "preparing"
-  #  end
-  #    
-  #    it "should not allow cancel an order if already paid" do
-  #      order = create_order
-  #      order.request.as('application/vnd.restbucks+xml').pay(payment(order.cost))
-  #      order.cancel.web_response.code.should == "405"
-  #    end
-  #    
-  #    it "should not allow to pay twice" do
-  #      order = create_order
-  #      order.request.as('application/vnd.restbucks+xml').pay(payment(order.cost))
-  #      order.request.as('application/vnd.restbucks+xml').pay(payment(order.cost)).web_response.code.should == "405"
-  #    end
-  #      
-  #   it "should allow to take out and receive receipt" do
-  #     order = create_order
-  #     order.request.as('application/vnd.restbucks+xml').pay(payment(order.cost), :method => :post)
-  #     sleep 20
-  #     order = order.self
-  #     order.status.should == "ready"
-  #     order.retrieve(:method => :delete)
-  #     Restfulie.cache_provider.clear
-  #     order = order.self
-  #     order.status.should == "delivered"
-  #     receipt = order.receipt
-  #     receipt.amount.should == "20.0"
-  #   end
-  #   
-  #   it "should work with twitter" do
-  #     statuses = Restfulie.at("http://twitter.com/statuses/public_timeline.xml").get
-  #     statuses.each do |status|
-  #       puts "#{status.user.screen_name}: #{status.text}, #{status.created_at}"
-  #     end
-  #   end
-  # 
+  it "should allow crawling children" do
+    order = create_order
+    items = order.items(:method => :get)
+    items[1].drink.should == "latte"
+  end
+  
+  it "should load from cache" do
+    order = create_order
+    previous_response = order.self.web_response
+    order.self.web_response.should == previous_response
+  end
+  
+   it "should cancel an order and delete it from the database" do
+     order = create_order
+     cancelled = order.cancel
+     cancelled.web_response.should be_is_successful
+     order.self.web_response.code.should == "404"
+   end
+   
+   # it "should update an order while possible" do
+   #   order = create_order
+   #   puts order.web_response.body
+   #   puts "foi o que eu tinha---------"
+   #   order = order.update(new_order("EAT_IN"), :method => :put)
+   #   order.web_response.is_successful?.should be_true
+   #   puts "peguei..."
+   #   puts order.web_response.body
+   #   puts "peguei..."
+   #   order.location.should == "EAT_IN"
+   # end
+
+   it "should complain if partially paying" do
+     order = create_order
+     order.request.as('application/vnd.restbucks+xml').payment(payment(1)).web_response.code.should == "400"
+   end  
+   
+   it "should allow to pay" do
+     order = create_order
+     puts order.web_response.body
+     order.request.as('application/vnd.restbucks+xml').payment(payment(order.cost)).web_response.code.should == "200"
+     order = order.self
+     order.web_response.code.should == "200"
+     order.status.should == "preparing"
+   end
+     
+     it "should not allow cancel an order if already paid" do
+       order = create_order
+       order.request.as('application/vnd.restbucks+xml').payment(payment(order.cost))
+       order.cancel.web_response.code.should == "405"
+     end
+     
+     it "should not allow to pay twice" do
+       order = create_order
+       order.request.as('application/vnd.restbucks+xml').payment(payment(order.cost))
+       order.request.as('application/vnd.restbucks+xml').payment(payment(order.cost)).web_response.code.should == "405"
+     end
+       
+    it "should allow to take out and receive receipt" do
+      order = create_order
+      order.request.as('application/vnd.restbucks+xml').payment(payment(order.cost), :method => :post)
+      sleep 20
+      order = order.self
+      order.status.should == "ready"
+      order.retrieve(:method => :delete)
+      Restfulie.cache_provider.clear
+      order = order.self
+      order.status.should == "delivered"
+      receipt = order.receipt(:method => :get)
+      receipt.amount.to_f.should == 20
+    end
+    
+    it "should work with twitter" do
+      statuses = Restfulie.at("http://twitter.com/statuses/public_timeline.xml").get
+      statuses.each do |status|
+        puts "#{status.user.screen_name}: #{status.text}, #{status.created_at}"
+      end
+    end
+  
   
 end
